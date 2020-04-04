fc2719e6c6a9e7c4277af1ecddd0976160cc4cb8
israel <israel.souza@inss.gov.br>
2020-04-04 15:54:13 -0300

    ajustes

--- src/web-service/IFeiraWS.js
+++ src/web-service/IFeiraWS.js
@@ -1,59 +1,76 @@
-const express = require('express');
-const authMiddleware = require('./middleware/auth');
-const SessionController = require('./controllers/SessionController');
-const FeiranteController = require('./controllers/FeiranteController');
-const RegioesController = require('./controllers/RegioesController');
+const express = require("express");
+const authMiddleware = require("./middleware/auth");
+const SessionController = require("./controllers/SessionController");
+const FeiranteController = require("./controllers/FeiranteController");
+const RegioesController = require("./controllers/RegioesController");
 
-require('dotenv').config({
-    path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://ifeira:LP979Riar3B6HTKS@cluster0-uybm2.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+const client = new MongoClient(url);
+const dbName = "ifeira";
+
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env",
 });
 
 class IFeiraWS {
+  #express;
 
-    #express;
+  constructor() {
+    this.#express = express();
+    this.#middlewares();
+    this.#routes();
+  }
 
-    constructor(){
-        this.#express = express();
-        this.#middlewares();
-        this.#routes();
+  get express() {
+    // precisa expor o express para usar nos testes
+    return this.#express;
+  }
+
+  ouvir(porta) {
+    if (!/^\d+$/.test(porta)) {
+      throw new TypeError("Porta informada deve ser numérica");
     }
+    this.#express.listen(porta);
+  }
 
-    get express(){
-        // precisa expor o express para usar nos testes
-        return this.#express;
-    }
+  #middlewares = () => {
+    this.#express.use(express.json());
+  };
 
-    ouvir(porta){
-        if(!/^\d+$/.test(porta)){
-            throw new TypeError("Porta informada deve ser numérica");
-        }
-        this.#express.listen(porta);
-    }
+  #routes = () => {
+    const routes = express.Router();
+    this.#express.use(routes);
 
-    #middlewares = ()=>{
-        this.#express.use(express.json());
-    }
+    routes.get(
+      "/feirantes/bairro/idBairro",
+      FeiranteController.listarPorBairro
+    );
 
-    #routes = ()=>{
-        const routes = express.Router();
-        this.#express.use(routes);
-        
-        routes.get('/feirantes/bairro/idBairro', FeiranteController.listarPorBairro);
-        routes.get('/feirante/id', FeiranteController.ler);
-        routes.put('/municipios/idEstado', RegioesController.listarMunicipiosPorEstado);
-        routes.put('/bairros/idMunicipio', RegioesController.listarBairrosPorMunicipio);
-        
-        routes.post('/sessions', SessionController.store);
-        
-        // middleware aplicado para as rotas abaixo
-        routes.use(authMiddleware);
-        
-        routes.post('/feirante', FeiranteController.gravar);
-        routes.put('/feirante/id', FeiranteController.atualizar);
-        routes.get('/painel/', FeiranteController.dadosPainel);
-        
-    }
+    routes.get("/feirante/id", FeiranteController.ler);
 
+    routes.get("/estados", RegioesController.listarEstados);
+
+    routes.put(
+      "/municipios/idEstado",
+      RegioesController.listarMunicipiosPorEstado
+    );
+    routes.put(
+      "/bairros/idMunicipio",
+      RegioesController.listarBairrosPorMunicipio
+    );
+
+    routes.post("/sessions", SessionController.store);
+
+    // middleware aplicado para as rotas abaixo
+    routes.use(authMiddleware);
+
+    routes.post("/feirante", FeiranteController.gravar);
+    routes.put("/feirante/id", FeiranteController.atualizar);
+    routes.get("/painel/", FeiranteController.dadosPainel);
+  };
 }
 
 module.exports = IFeiraWS;
--- src/web-service/controllers/RegioesController.js
+++ src/web-service/controllers/RegioesController.js
@@ -1,13 +1,52 @@
 class RegioesController {
+  async listarEstados(req, res) {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
 
-    async listarMunicipiosPorEstado(req, res){
-        
+      // Use the collection "people"
+      const collection = db.collection("localidades");
+      const p = await collection.aggregate([
+        { $group: { _id: { uf: "$uf", nome_estado: "$nome_estado" } } },
+      ]);
+      res(p);
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      await client.close();
     }
+  }
 
-    async listarBairrosPorMunicipio(req, res){
+  async listarMunicipiosPorEstado(req, res) {
+    await db.getCollection("localidades").aggregate([
+      { $match: { nome_estado: "minas gerais" } },
+      {
+        $group: {
+          _id: {
+            uf: "$uf",
+            nome_estado: "$nome_estado",
+            nome_municipio: "$nome_municipio",
+          },
+        },
+      },
+    ]);
+  }
 
-    }
-
+  async listarBairrosPorMunicipio(req, res) {
+    await db.getCollection("localidades").aggregate([
+      { $match: { nome_estado: "minas gerais", nome_municipio: "contagem" } },
+      {
+        $group: {
+          _id: {
+            uf: "$uf",
+            nome_estado: "$nome_estado",
+            nome_municipio: "$nome_municipio",
+            bairros: "$bairros",
+          },
+        },
+      },
+    ]);
+  }
 }
 
-module.exports = new RegioesController;
+module.exports = new RegioesController();
