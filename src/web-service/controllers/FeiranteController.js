b6d3b8311134ae1dfa148b1bc80d8499de461426
israel <israel.souza@inss.gov.br>
2020-04-06 11:07:15 -0300

    ajuste para mongodb

--- src/web-service/controllers/FeiranteController.js
+++ src/web-service/controllers/FeiranteController.js
@@ -1,36 +1,139 @@
-const FeiranteFactory = require('../feirante/FeiranteFactory');
+const FeiranteFactory = require("../feirante/FeiranteFactory");
 
 class SessionController {
+  async gravar(req, res) {
+    let feirante;
+    try {
+      feirante = FeiranteFactory.fromObject(req.body);
 
-    async gravar(req, res){
-        let feirante;
-        try{
-            feirante = FeiranteFactory.fromObject(req.body);
-            
-            return res.json(feirante);
-        }catch(e){
-            console.log(e);
-            return res.status(500).json({message: "Erro ao consultar o banco de dados"});
+      return res.json(feirante);
+    } catch (e) {
+      console.log(e);
+      return res
+        .status(500)
+        .json({ message: "Erro ao consultar o banco de dados" });
+    }
+  }
+
+  //paulo henrique achou
+  async ler(req, res) {
+    try {
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+
+        const collection = db.collection("feirante");
+        const p = collection;
+        db.getCollection("feirantes")
+          .find({ email: "edson_yamada@gmail.com" })
+          .toArray(function (err, result) {
+            if (err) {
+              console.log(err);
+            }
+            res.json(result);
+          });
+      } catch (err) {
+        console.log(err.stack);
+      }
+    } catch (e) {
+      console.log(e);
+      return res
+        .status(500)
+        .json({ message: "Erro ao consultar o banco de dados" });
+    }
+  }
+
+  async atualizar(req, res) {
+    try {
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+
+        const collection = db.collection("feirante");
+        const p = collection;
+
+        // set vc coloca todos os campos q quer atualizar
+
+        db.getCollection("feirantes")
+          .update({ email: "edson_yamada@gmail.com" }, { $set: { nome: "zl" } })
+          .toArray(function (err, result) {
+            if (err) {
+              console.log(err);
+            }
+            res.json(result);
+          });
+      } catch (err) {
+        console.log(err.stack);
+      }
+    } catch (e) {
+      console.log(e);
+      return res
+        .status(500)
+        .json({ message: "Erro ao consultar o banco de dados" });
+    }
+  }
+
+  async listarPorBairro(req, res) {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+
+      // Use the collection "people"
+      const collection = db.collection("feirantes");
+      const p = collection
+        .find({ bairros: "ro:rondonia:altaflorestadoeste:centroareaflor" })
+        .toArray(function (err, result) {
+          if (err) {
+            console.log(err);
+          }
+          res.json(result);
+        });
+    } catch (err) {
+      console.log(err.stack);
+    }
+  }
+
+  async dadosPainel(req, res) {
+    return res.status(200).json({ message: "a implementar" });
+  }
+
+  /* Documentacao do código: Israel 06/04/2020 
+  Cmo nao tem controlador pra isso deixei aqui e depois o backender decide o que fazer */
+  async listarProdutos(req, res) {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+
+      // Use the collection "people"
+      const collection = db.collection("produtos");
+      const p = collection.find({}).toArray(function (err, result) {
+        if (err) {
+          console.log(err);
         }
+        res.json(result);
+      });
+    } catch (err) {
+      console.log(err.stack);
     }
+  }
 
-    async ler(req, res){
+  async listarTiposProdutos(req, res) {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
 
+      // Use the collection "people"
+      const collection = db.collection("tiposProdutos");
+      const p = collection.find({}).toArray(function (err, result) {
+        if (err) {
+          console.log(err);
+        }
+        res.json(result);
+      });
+    } catch (err) {
+      console.log(err.stack);
     }
-
-    async atualizar(req, res){
-        
-    }
-
-    async listarPorBairro(req, res){
-
-    }
-
-    async dadosPainel(req, res){
-        return res.status(200).json({message: "a implementar"});
-    }
-
-
+  }
 }
 
 module.exports = new SessionController();
--- src/web-service/controllers/SessionController.js
+++ src/web-service/controllers/SessionController.js
@@ -1,39 +1,64 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://ifeira:LP979Riar3B6HTKS@cluster0-uybm2.gcp.mongodb.net/test?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+const dbName = "ifeira";
 
 class SessionController {
-    async store(req, res){
-        const { usuario, senha } = req.body;
+  async store(req, res) {
+    const { usuario, senha } = req.body;
 
-        let loginValido;
-        try{
-            // consultar no banco e para buscar os dados do usuário e hash da senha;
-        }catch(e){
-            console.log(e);
-            return res.status(500).json({message: "Erro ao consultar o banco de dados"});
-        }
+    let loginValido;
+    try {
+      try {
+        await client.connect();
+        const db = client.db(dbName);
 
-        const hashDaSenhaTeste = await bcrypt.hash("teste", 8); 
-        const senhaConfere = await bcrypt.compare(senha, hashDaSenhaTeste);
-        const usuarioConfere = usuario === "teste";
-        
-        if(usuarioConfere && senhaConfere){
-            loginValido = true;
-        }else{
-            loginValido = false;
-        }
-
-        if(!loginValido){
-            return res.status(401).json({message: 'Login inválido'});
-        }
-
-        const dadosDoToken = { id: usuario };
-        const token = jwt.sign(dadosDoToken, process.env.APP_SECRET);
-
-        return res.status(200).json({
-            token: token 
-        });
+        const collection = db.collection("feirante");
+        const p = collection;
+        db.getCollection("feirantes")
+          .find({ email: "edson_yamada@gmail.com" })
+          .toArray(function (err, result) {
+            if (err) {
+              console.log(err);
+            }
+            res.json(result);
+          });
+      } catch (err) {
+        console.log(err.stack);
+      }
+    } catch (e) {
+      console.log(e);
+      return res
+        .status(500)
+        .json({ message: "Erro ao consultar o banco de dados" });
     }
+
+    const hashDaSenhaTeste = await bcrypt.hash("teste", 8);
+    const senhaConfere = await bcrypt.compare(senha, hashDaSenhaTeste);
+    const usuarioConfere = usuario === "teste";
+
+    if (usuarioConfere && senhaConfere) {
+      loginValido = true;
+    } else {
+      loginValido = false;
+    }
+
+    if (!loginValido) {
+      return res.status(401).json({ message: "Login inválido" });
+    }
+
+    const dadosDoToken = { id: usuario };
+    const token = jwt.sign(dadosDoToken, process.env.APP_SECRET);
+
+    return res.status(200).json({
+      token: token,
+    });
+  }
 }
 
 module.exports = new SessionController();
